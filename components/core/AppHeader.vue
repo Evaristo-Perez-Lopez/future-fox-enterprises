34027c2cc65e369e6a2fa74922381f871980a956
Evaristo Perez <perezlopezevaristo07@outlook.com>
2023-04-04 19:56:55 -0500

    initial settings

--- components/core/AppHeader.vue
+++ components/core/AppHeader.vue
@@ -3,7 +3,9 @@
 <template>
   <header class="w-full bg-white h-20 border-b-2 shadow shadow-blue-100">
     <div class="max-w-4xl w-min h-full mx-auto flex items-center">
-      <h1 class="font-bold text-center text-xl border-2">Technical Test</h1>
+      <NuxtLink to="/" class="font-bold text-center text-xl border-2">
+        Technical Test
+      </NuxtLink>
     </div>
   </header>
 </template>
