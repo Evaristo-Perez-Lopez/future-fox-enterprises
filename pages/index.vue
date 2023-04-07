b12d8f7b07175f08d426646566a93f8b6ae8380f
Evaristo Perez <perezlopezevaristo07@outlook.com>
2023-04-07 10:34:02 -0500

    search results bar

--- pages/index.vue
+++ pages/index.vue
@@ -14,6 +14,13 @@ function send() {
     <section class="text-gray-600 px-4 mb-8">
       <CoreFormsSearchBar />
     </section>
+    <section class="px-4">
+      <article class="bg-sky-100 px-3 py-2">
+        <h4 class="text-sky-600 font-normal text-md md:text-lg">
+          Search results: <span class="font-bold">0</span>
+        </h4>
+      </article>
+    </section>
     <h1>Starting Project</h1>
     <p>Search App</p>
     <input type="text" class="w-96 h-40 border-2 text-black" v-model="msg" />
